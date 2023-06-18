c9f216b161bdf96f7caeaa4096ad03d82c9dae4f
JorgeReyes115 <cocoreyes_2011@hotmail.com>
2023-06-18 17:23:56 -0300

    commit

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const inquirer =("inquirer");
 const fs = ("fs");
 const questions = require("./lib/questions.js");
 const shapeRender = require("./lib/shapeRender.js")
+
+
+function createLogo(data) {
+    fs.writeFile("./examples/logo.svg", shapeRender, ()=> console.log("logo.svg has been generated"));
+}
+
+
+function init(){
+    inquirer
+    .prompt(questions)
+    .then((data)=>{
+        createLogo(data);
+    });
+}
+
+
+init();
